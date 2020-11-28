fba338a0f64e1aa1f684b1abc2e17272f85f2a7b
unknown <jamessamantha099@gmail.com>
2020-11-28 17:22:12 -0600

    added account

---
+++ TestInfo/account.sql
@@ -0,0 +1,13 @@
+INSERT INTO User 
+ VALUES
+	('TRE0','Robert','Reid',' 16 Tangerine Pl Kingston 10','5','Owner','11500'),
+	('TRE1','Justin','Alvarez','935 W. Depot St.Fort Wayne IN 46804','16','Employee','10000'),
+	('TRE2','Mark','Peters','615 Border Dr.Grayslake IL 60030','22','Employee','8000'),
+	('TRE3','Joseph','Wagner','30 Knutsford Blvd','16','Employee','11500'),
+	('TRE4','Erica','Martin','14a Lady Musgrave Rd Kingston 5','11','Employee','8000'),
+	('TRE5','Robert','Martinez','615 Border Dr.Grayslake IL 60030','18','Employee','11000'),
+	('TRE6','Anita','Long','615 Border Dr.Grayslake IL 60030','5','Employee','9500'),
+	('TRE7','Lisa','Beck','21 Mannings Hill Rd Kingston 8','3','Employee','11500'),
+	('TRE8','Jeremy','Carr','14a Lady Musgrave Rd Kingston 5','7','Employee','10500'),
+	('TRE9','Deanna','Andersen','22 Little Premier Plaza Kingston 10Gordon Town Road Kingston 6','19','Employee','8500'),
+	('TRE10','Lisa','Taylor','29 Passagefort Dr Portmore','9','Employee','11500');
---
+++ TestInfo/owner.sql
@@ -0,0 +1,1 @@
+insert into Owner values('TRE0', 'Robert Reid');
