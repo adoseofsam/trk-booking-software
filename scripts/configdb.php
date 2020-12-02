922500118d34521ecfa74392e1168165b4cbaa20
unknown <jamessamantha099@gmail.com>
2020-12-01 22:20:29 -0600

    php files to connect db

---
+++ scripts/configdb.php
@@ -0,0 +1,12 @@
+<?php
+    $host = '127.0.0.1';
+    $username = 'admin';
+    $password = 'trk_online';
+    $dbname = 'TrkOnlineBooking';
+    $charset = 'utf8mb4';
+<<<<<<< HEAD
+
+    
+    
+=======
+>>>>>>> manage-account
