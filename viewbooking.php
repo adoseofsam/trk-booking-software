f365153254cd85d8dad4da6755e2cf4cb74eadaf
xkristina <75180523+xkristina@users.noreply.github.com>
2020-12-01 22:02:11 -0500

    Add files via upload

---
+++ viewbooking.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>View Booking</title>
+<link rel="stylesheet" href="viewb.css"> 
+<script type="text/javascript" src="viewb.js"></script>
+</head>
+
+<body>
+
+  <input type="text" id="search" onkeyup="myFunction()" placeholder="Search for Booking" title="Type a Booking Name">
+
+  <table id="bookingInfo">
+
+    <tr class="header">
+        <th>Event</th>
+        <th>Client</th>
+        <th>Venue</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Equipment</th>
+    </tr>
+
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "testerdb";
+
+     
+        $conn = new mysqli($servername, $username, $password, $dbname);
+            
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT Event, Client, Venue, Date, Time, Equipment  FROM testdata";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            
+            while($row = $result-> fetch_assoc()) {
+                echo "<tr><td>" . $row["Event"]. "</td><td>" . $row["Client"]. "</td><td>" . $row["Venue"]. "</td><td>" . $row["Date"]. "</td><td>" . $row["Time"]. "</td><td>" . $row["Equipment"]. "</td><td>" . "</td></tr>";
+            }
+            echo "</table>";
+            } else {
+            echo "0 results";
+            }
+
+        $conn->close();
+        ?>
+
+
+
+  </table>
+           
+
+</body>
+
+</html>
+
+
